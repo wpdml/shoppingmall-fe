bdfc9e4f4487c78b0ded6861a3d4a9ce9a8494c1
이제의 <jeui@ije-uis-MacBook-Air.local>
2024-10-26 06:05:11 +0900

    registration

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,80 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import { showToastMessage } from "../common/uiSlice";
+import api from "../../utils/api";
+import { initialCart } from "../cart/cartSlice";
+
+export const loginWithEmail = createAsyncThunk(
+  "user/loginWithEmail",
+  async ({ email, password }, { rejectWithValue }) => {}
+);
+
+export const loginWithGoogle = createAsyncThunk(
+  "user/loginWithGoogle",
+  async (token, { rejectWithValue }) => {}
+);
+
+export const logout = () => (dispatch) => {};
+
+export const registerUser = createAsyncThunk(
+  "user/registerUser",
+  async (
+    { email, name, password, navigate },
+    { dispatch, rejectWithValue }
+  ) => {
+    try {
+      const response = await api.post("/user", { email, name, password });
+      dispatch(
+        showToastMessage({
+          message: "registration successful!",
+          status: "success",
+        })
+      );
+      navigate("/login");
+
+      return response.data.data;
+    } catch (error) {
+      dispatch(
+        showToastMessage({ message: "registration failed", status: "error" })
+      );
+      return rejectWithValue(error.error);
+    }
+  }
+);
+
+export const loginWithToken = createAsyncThunk(
+  "user/loginWithToken",
+  async (_, { rejectWithValue }) => {}
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+    loading: false,
+    loginError: null,
+    registrationError: null,
+    success: false,
+  },
+  reducers: {
+    clearErrors: (state) => {
+      state.loginError = null;
+      state.registrationError = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.loading = false;
+        state.registrationError = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.registrationError = action.payload;
+      });
+  },
+});
+export const { clearErrors } = userSlice.actions;
+export default userSlice.reducer;
